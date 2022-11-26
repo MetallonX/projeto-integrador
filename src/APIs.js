0895f5a96fc430f283c3d1fc461e44e7429c0382
Lucas Costa de Andrade <81720478+MetallonX@users.noreply.github.com>
2022-11-26 10:05:30 -0300

    Update APIs.js

--- src/APIs.js
+++ src/APIs.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'APIKEY',
+        'X-RapidAPI-Key': '84cc7bfa6amsh3a853f15de8ab35p1d14d9jsn865c4bb38a46',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
@@ -9,5 +9,5 @@ export const geoApiOptions = {
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = 'APIKEY';
+export const WEATHER_API_KEY = '55705581bf0001e72091cbf1e2163f49';
 
